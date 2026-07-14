478b54c348f07ad72eaae903e7f606b59aef6a32
Vic Valenzuela <victor@dala.care>
2026-07-14 20:27:56 +0000

    fix: scope favourite controls to place mode

--- src/lib/favourites/FavouriteControl.svelte
+++ src/lib/favourites/FavouriteControl.svelte
@@ -68,6 +68,7 @@
 
 <div
   class="favourite-action"
+  data-ui-mode="place"
   data-favourite-place={placeId}
   data-state={failed ? 'error' : submitting ? 'busy' : favourite ? 'selected' : 'idle'}
 >
